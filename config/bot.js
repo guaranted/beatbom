46d0bc8ef5e583adee6748ca710c45374dec75d8
guaranted <73685531+guaranted@users.noreply.github.com>
2021-01-13 15:48:32 +0300

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: '<:carpi:798449451403706378>  ',
+        error: '<:carpi:798449451403706378>  ',
+        queue: '<:ListePng:708215766754918512>  ',
+        music: ' <:muzik:798803622686621706>  ',
+        success: '<:tik:798449162441588756> ',
+    },
+
+    discord: {
+        token: 'NzczMjU2NDczODY2MTQxNzI5.X6GlNQ.1cTCQxZPgmC9sl_xNbkIHHPRNH0',
+        prefix: '!',
+        activity: 'biz türkler kdv öderiz',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
